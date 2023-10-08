eb5668f836b265953f5dc50e22f9204f9f15408b
viktors <houdinielement@gmail.com>
2023-10-08 16:30:53 +0300

    OP menu item for setting string attribute from nodes name

--- scripts/python/MASSE_tools/parm_utils.py
+++ scripts/python/MASSE_tools/parm_utils.py
@@ -1055,3 +1055,34 @@ def color_nodes():
         # check if node name contains delete_riggers
         if any(rigger in node.name().lower() for rigger in delete_riggers):
             node.setColor(delete_color)
+
+
+def attrib_from_node_name(kwargs):
+    selected_nodes = kwargs["items"]
+    button_list = ("Detail","Prim", "Point", "Cancel")
+    attrib_select = hou.ui.readInput("Enter attrib name", buttons=button_list,initial_contents="name")
+    selected_button = button_list[attrib_select[0]]
+    attrib_name = attrib_select[1]
+    # make sure attribute os only from letters
+    attrib_name = re.sub(r"[^a-zA-Z]", "_", attrib_name).strip("_")
+    if  attrib_name:
+        if selected_button != "Cancel":
+            for node in selected_nodes:
+                node_name = node.name()
+                node_name = re.sub(r"[^a-zA-Z0-9_]", "_", node_name)
+                node_name = node_name.strip("_")
+
+                # connect to output 0 name node
+                name_node = node.parent().createNode("attribwrangle", f"attrib_{attrib_name}")
+                name_node.setInput(0, node)
+                node_pos = node.position()
+                node_pos[1] += -1
+                name_node.setPosition(node_pos)
+
+                # set wrangle code
+                wrangle_code = f"s@{attrib_name} = \"{node_name}\";"
+                name_node.parm("snippet").set(wrangle_code)
+
+                # set attrib typle
+                name_node.parm("class").set(attrib_select[0])
+
