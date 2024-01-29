70796081b213e745f5767d7813421a7720f9523b
viktors-a <houdinielement@gmail.com>
2024-01-30 01:56:50 +0200

    NEW preset for group node, giving option to size bounding box from viewport selection

---
+++ scripts/python/MASSE_tools/scripts.py
@@ -0,0 +1,28 @@
+""" Assortment of useful functions"""
+import hou
+
+
+# function called on a spare button in a group node, for custom bbox size based on viewport selection.
+def get_viewport_selection_bbox(kwargs):
+    scane_viewer = hou.ui.paneTabOfType(hou.paneTabType.SceneViewer)
+    parent_node = kwargs["node"]
+    if scane_viewer:
+        geo_selection = scane_viewer.currentGeometrySelection()
+        if geo_selection:
+            selected_nodes = geo_selection.nodes()
+            # make sure that selection is only from node that calls this function
+            if len(selected_nodes) == 1:
+                if parent_node == selected_nodes[0]:
+                    selection_bbox = geo_selection.boundingBox()
+                    size_parms = parent_node.parmTuple("size")
+                    scale_parms = parent_node.parmTuple("bbox_multiplier")
+                    bbox_sizes = selection_bbox.sizevec()
+                    # set multiplier and bbox size
+                    if size_parms and scale_parms:
+                        for size_parm, scale_parm, bbox_size in zip(size_parms, scale_parms, bbox_sizes):
+                            size_parm.set(bbox_size * scale_parm.eval())
+                        # set center
+                        parent_node.parmTuple("t").set(selection_bbox.center())
+            else:
+                parent_node.parm("initbounds").pressButton()
+
